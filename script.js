771a51539d031d3359b9d09df5b26752a776b4ae
sivasankargomasani <gomasanisivasankar6281@gmail.com>
2024-04-28 23:37:01 +0530

    "first commit"

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendValue(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display').value;
+    document.getElementById('display').value = display.substring(0, display.length - 1);
+}
+
+function calculateResult() {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (e) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
